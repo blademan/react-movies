ea91172677b64cbccd638505c26f855addb94e2a
Eduard <blademan82@gmail.com>
2022-07-06 19:27:18 +0300

    update links

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 export default function Footer() {
   return (
     <>
-      <footer className="page-footer purple accent-3">
+      <footer className="page-footer">
         <div className="footer-copyright">
           <div className="container">
             © {new Date().getFullYear()} Copyright Text
-            <a className="grey-text text-lighten-4 right" href="#!">
+            <a
+              className="grey-text text-lighten-4 right"
+              href="https://github.com/blademan/react-movies">
               Repo
             </a>
           </div>
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <>
       <nav className=" purple accent-2">
         <div className="nav-wrapper">
-          <a href="https://blademan.github.io/react-movies/" className="brand-logo">
-            React Movies
-          </a>
+          <Link to="/"> React Movies</Link>
+
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li>
               <a href="https://github.com/blademan/react-movies">Repo</a>
